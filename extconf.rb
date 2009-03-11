254a50203fb1637aaec4c44ce619ff5504a1f419
Sean Bradly <sean@phi.(none)>
2009-03-11 16:22:45 -0500

    Cleaned extconf up.   added default path to headers   added '--with-debug' option

--- extconf.rb
+++ extconf.rb
@@ -1,10 +1,27 @@
 require 'mkmf'
 
-$CFLAGS = (ENV['CFLAGS']||"") + "-Wall -g"
-$LDFLAGS = (ENV["LDFLAGS"]||"")+"-lvixAllProducts"
+$CFLAGS = (ENV['CFLAGS']||"") + "-Wall"
 
-have_library('vixAllProducts','VixHost_Connect')
+# allow --with-vixapi-include to set an include path
+dir_config("vixapi")
+# default include directory
+$CFLAGS += " -I/usr/include/vmware-vix"
 
-dir_config("vixapi","/usr/include/vmware-vix")
+vix_so = "vixAllProducts"
+
+# compile with debugging symbols
+if with_config('debug')
+	$CFLAGS += " -g"
+	# vix_so = "vixAllProductsd" (referenced in the doc, not there on linux)
+end
+
+# Check VIX lib
+if not have_library(vix_so,'VixHost_Connect')
+	puts "Can't find the Vix library"
+	exit(-1)
+end	
+
+# Link to vix
+$LDFLAGS = (ENV["LDFLAGS"]||"")+" -l#{vix_so}"
 
 create_makefile("vixapi")
